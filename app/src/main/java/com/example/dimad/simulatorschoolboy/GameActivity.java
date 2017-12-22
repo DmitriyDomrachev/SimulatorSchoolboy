82b9b4a424e2c7b31ead6ce0ee09bf2cabb69472
DmitriyDomrachev <dima200214@gmail.com>
2017-12-22 09:34:04 +0500

    2

--- app/src/main/java/com/example/dimad/simulatorschoolboy/GameActivity.java
+++ app/src/main/java/com/example/dimad/simulatorschoolboy/GameActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 public class GameActivity extends AppCompatActivity {
     private static TextView titleTextView;
     private static TextView situationTextView;
+    public static Character player;
+    public static Story story;
     Button firstAns, secondAns;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,11 @@ public class GameActivity extends AppCompatActivity {
         titleTextView = findViewById(R.id.titleTextView);
         situationTextView = findViewById(R.id.situationTextView);
         titleTextView.setText(getString(R.string.titleFirst));
-        situationTextView.setText(MainActivity.PlayerName+", "+getString(R.string.situationText));
+        player = new Character(getIntent().getStringExtra("name"));
+
+        situationTextView.setText(player.name+", "+getString(R.string.situationText));
+        story = new Story();
+        
 
     }
 }
--- app/src/main/java/com/example/dimad/simulatorschoolboy/MainActivity.java
+++ app/src/main/java/com/example/dimad/simulatorschoolboy/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.dimad.simulatorschoolboy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static Character palyer;
-    public static Story story;
+
     private static EditText playerName;
     public static String PlayerName;
+    Button startGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         playerName = findViewById(R.id.nameEditText);
         PlayerName = String.valueOf(playerName.getText());
+        startGame = findViewById(R.id.startGameButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                switch (v.getId()) {
+                    case R.id.startGameButton:
+                        i = new Intent(MainActivity.this, GameActivity.class);
+                        i.putExtra("name",playerName.getText().toString());
+                        startActivity(i);
+                        break;
+
+                    default:
+                        return;
+                }
+            }
+        };
+        startGame.setOnClickListener(listener);
     }
 
     protected void onResume(){
